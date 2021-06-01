05be0ef5b6a783eb2d573a325945a230fc450e40
lgriffie <loic@griffie.fr>
2021-06-01 18:36:29 +0200

    Add Session.Form

--- Sources/Hubkit/Models/Session+Content.swift
+++ Sources/Hubkit/Models/Session+Content.swift
@@ -2,3 +2,19 @@ import Vapor
 import HubkitModel
 
 extension HubkitModel.Session: Content {}
+
+public extension HubkitModel.Session {
+    struct Form: Content {
+        public let project: UUID
+        public let capturedAt: Date
+        public let callbackUrl: String?
+        public let metas: [String]?
+
+        public init(project: UUID, capturedAt: Date, callbackUrl: String? = nil, metas: [String]? = nil) {
+            self.project = project
+            self.capturedAt = capturedAt
+            self.callbackUrl = callbackUrl
+            self.metas = metas
+        }
+    }
+}
